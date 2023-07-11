2de1765299320fb8654f3ec75cb2f3d20eeee814
Gleicilene <1410522@sga.pucminas.br>
2023-07-11 15:37:34 -0300

    Find Category by id e Exception handling

--- backend/src/main/java/com/skgestao/sksistemagestao/services/CategoryService.java
+++ backend/src/main/java/com/skgestao/sksistemagestao/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.skgestao.sksistemagestao.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.skgestao.sksistemagestao.dto.CategoryDTO;
 import com.skgestao.sksistemagestao.entities.Category;
 import com.skgestao.sksistemagestao.repositories.CategoryRepository;
+import com.skgestao.sksistemagestao.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -21,6 +23,14 @@ public class CategoryService {
 	public List<CategoryDTO> findAll(){
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
+		
 	}
 
 }
