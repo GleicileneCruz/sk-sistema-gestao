c3e03d832b57a083bec90b863c9ce94c72eb61ba
Gleicilene <1410522@sga.pucminas.br>
2023-07-11 11:57:47 -0300

    DTO

--- backend/src/main/java/com/skgestao/sksistemagestao/services/CategoryService.java
+++ backend/src/main/java/com/skgestao/sksistemagestao/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.skgestao.sksistemagestao.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.skgestao.sksistemagestao.dto.CategoryDTO;
 import com.skgestao.sksistemagestao.entities.Category;
 import com.skgestao.sksistemagestao.repositories.CategoryRepository;
 
@@ -16,8 +18,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
