b7035aa5e07378d08fec2084ce4126ddde583f35
Gleicilene <1410522@sga.pucminas.br>
2023-07-13 16:07:05 -0300

    Custom validation response

--- backend/src/main/java/com/skgestao/sksistemagestao/dto/ProductDTO.java
+++ backend/src/main/java/com/skgestao/sksistemagestao/dto/ProductDTO.java
@@ -9,14 +9,28 @@ import java.util.Set;
 import com.skgestao.sksistemagestao.entities.Category;
 import com.skgestao.sksistemagestao.entities.Product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ProductDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@Size(min = 5, max = 70, message = "Deve ter entre 5 e 70 caracteres")
+	@NotBlank(message = "Campo obrigatório")
 	private String name;	
+	
+	@NotBlank(message = "Campo requerido")
 	private String description;
+	
+	@Positive(message = "Preço deve ser um valor positivo")
 	private Double price;
-	private String imgUrl;	
+	private String imgUrl;
+	
+	@PastOrPresent(message = "A data do produto não pode ser futura")
 	private Instant date;
 	
 	private List<CategoryDTO> categories = new ArrayList<>();
--- backend/src/main/java/com/skgestao/sksistemagestao/resources/UserResource.java
+++ backend/src/main/java/com/skgestao/sksistemagestao/resources/UserResource.java
@@ -16,40 +16,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.skgestao.sksistemagestao.dto.ProductDTO;
-import com.skgestao.sksistemagestao.services.ProductService;
+import com.skgestao.sksistemagestao.dto.UserDTO;
+import com.skgestao.sksistemagestao.dto.UserInsertDTO;
+import com.skgestao.sksistemagestao.services.UserService;
 
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping(value = "/products")
-public class ProductResource {
+@RequestMapping(value = "/users")
+public class UserResource {
 	
 	@Autowired
-	private ProductService service;
+	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
-		Page<ProductDTO> list = service.findAllPaged(pageable);
+	public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable){
+		Page<UserDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
-		ProductDTO dto = service.findById(id);
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+		UserDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
 	
 	@PostMapping
-	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto){
-		dto = service.insert(dto);
+	public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO dto){
+		UserDTO newDto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(dto.getId()).toUri();
-		return ResponseEntity.created(uri).body(dto);
+				.buildAndExpand(newDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto);
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto){
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserDTO dto){
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
